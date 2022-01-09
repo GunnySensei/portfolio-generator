e02edfc1ae7258a1284a6c5062695f577a105bc1
GunnySensei <gunnerwagoner@hotmail.com>
2022-01-09 14:38:02 -0600

    importing inquirer

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
-const profileDataArgs = process.argv.slice(2);
+const inquirer = require('inquirer');
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js');
 
-const [userName, githubName] = profileDataArgs;
+// const pageHTML = generatePage(userName, githubName);
 
-const fs = require('fs');
-const generatePage = require('./src/page-template.js')
+// fs.writeFile('./index.html', pageHTML, err => {
+//     if(err) throw err; 
 
-fs.writeFile('./index.html', generatePage(userName, githubName), err => {
-    if(err) throw new Error (err); 
+//     console.log('Portfolio complete! Check out index.html to see the output!')
+// });
 
-    console.log('Portfolio complete! Check out index.html to see the output!')
-});
-
+inquirer 
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        }
+    ])
+    .then(answers => console.log(answers));
